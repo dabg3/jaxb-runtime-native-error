ad5a31fba549ea1cf9299bc2e4f828e78208b7a9
Davide Bigotti <dbigotti@omniagroup.it>
2022-12-05 14:54:17 +0100

    add reflection hint

--- src/test/java/org/example/JAXBTest.java
+++ src/test/java/org/example/JAXBTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.StringReader;
+
 @SpringBootTest
 public class JAXBTest {
 
@@ -14,9 +16,14 @@ public class JAXBTest {
     public void readXml() throws Exception {
         JAXBContext jaxbContext = JAXBContext.newInstance(HandleConn.class);
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        ClassPathResource file = new ClassPathResource("content.xml");
+        /* ClassPathResource is likely to miss hints as well.
+         * ClassPathResource file = new ClassPathResource("content.xml");
+         * replacing with StringReader as seen in:
+         * https://github.com/oracle/graalvm-reachability-metadata/blob/master/tests/
+         */
+        StringReader reader = new StringReader("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><HandleConn><SHandle>0000</SHandle></HandleConn>");
 
-        HandleConn handle = (HandleConn) unmarshaller.unmarshal(file.getInputStream());
+        HandleConn handle = (HandleConn) unmarshaller.unmarshal(reader);
 
         Assertions.assertThat(handle != null && !handle.getSHandle().isBlank());
         System.out.println("----HANDLE " + handle.getSHandle() + "----");
